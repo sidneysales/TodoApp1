312bba75a494cd312167bfb3909fc0e5a7304dd4
Sidney Sales <sidnelsom@gmail.com>
2018-05-14 19:27:43 -0300

    modified:   config.xml 	modified:   package-lock.json 	modified:   package.json 	modified:   www/index.html 	modified:   www/js/app.js 	new file:   www/templates/lista.html 	new file:   www/templates/novo.html 	www/css/ionic.app.css 	www/css/ionic.app.min.css

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,79 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('todoapp', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'templates/lista.html'
+  });
+
+  $stateProvider.state('new', {
+    url: '/new',
+    templateUrl: 'templates/novo.html'
+  });
+
+
+  $urlRouterProvider.otherwise('/list');
+});
+
+
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+var tarefas = [
+  {"texto": "Realizar as atividades do curso",
+  "data": new Date(),
+  "feita": false
+  },
+  {"texto": "Passear com o cachorro",
+  "data": new Date(),
+  "feita": true
+  }
+
+];
+
+app.controller('ListaCtrl', function($scope) {
+
+  //IMPLEMENTACAO DO CONTROLER, METODOS USADOS NA VIEW:
+  $scope.tarefas = tarefas;
+
+  $scope.concluir = function(indice) {
+    $scope.tarefas[indice] = true;
+  }
+
+  $scope.apagar = function(indice) {
+    $scope.tarefas.splice(indice);
+  }
+  
+});
+
+app.controller('NovoCtrl', function($scope, $state) {
+
+  $scope.salvar = function(){
+
+    var tarefa = {
+      "texto": $scope.texto, //<input ng-model="texto"...
+      "data": new Date(),
+      "feita": false
+    };
+
+    tarefas.push(tarefa);
+
+    $state.go('list');
+
+  }
+
+});
+
